e3e8631781e3e5f9ca7280d392291a0de16dc916
imani <18imani.vata@gmail.com>
2024-06-17 15:19:00 +0300

    dne

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,18 @@
 
 class Dog:
     # Class body goes here
+    def bark(self):
+        print( "Woof!")
 
+    def sit(self):
+        print("The dog is sitting.")    
     #Instance method definition
-    pass
+fido = Dog()
+fido.bark()
+
+snoppy = Dog()
+snoppy.bark()
+
+fido = Dog()
+fido.sit()
+
